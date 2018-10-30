992f4906ee2f4f3ef44788f6a9a369a5c6766b3c
bouncehh <bouncehh@users.noreply.github.com>
2018-10-30 13:58:26 -0300

    Update HelloService.java

--- src/main/java/io/veicot/openshift/rest/HelloService.java
+++ src/main/java/io/veicot/openshift/rest/HelloService.java
@@ -14,6 +14,6 @@ public class HelloService extends Application {
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from OpenShift! (feature/1)").build();
+		return Response.ok("Hello from OpenShift! (feature/1 30/oct)").build();
 	}
 }
